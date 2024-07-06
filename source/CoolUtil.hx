fb9e147b7065a2bcdca2adf0cf88d632284b05fb
8bit <139103755+RGBXD4@users.noreply.github.com>
2024-07-06 15:58:48 +0800

    Update CoolUtil.hx

--- source/CoolUtil.hx
+++ source/CoolUtil.hx
@@ -8,9 +8,8 @@ import lime.utils.AssetManifest;
 #if sys
 import sys.io.File;
 import sys.FileSystem;
-#else
-import openfl.utils.Assets;
 #end
+import openfl.utils.Assets;
 
 using StringTools;
 
@@ -51,12 +50,9 @@ class CoolUtil
 	public static function coolTextFile(path:String):Array<String>
 	{
 		var daList:Array<String> = [];
-		#if sys
-		if(FileSystem.exists(path)) daList = File.getContent(path).trim().split('\n');
-		#else
+	
 		if(Assets.exists(path)) daList = Assets.getText(path).trim().split('\n');
-		#end
-
+		
 		for (i in 0...daList.length)
 		{
 			daList[i] = daList[i].trim();
