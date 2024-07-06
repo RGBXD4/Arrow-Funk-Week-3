1a8ce78035c5f389bfd69ecd5ae2e9000e76c533
8bit <139103755+RGBXD4@users.noreply.github.com>
2024-07-06 19:28:36 +0800

    Update MainMenuState.hx

--- source/MainMenuState.hx
+++ source/MainMenuState.hx
@@ -233,6 +233,11 @@ class MainMenuState extends MusicBeatState
 		}
 		#end
 
+		#if android
+                addVirtualPad(UP_DOWN, A_B);
+		addPadCamera();
+                #end
+
 		super.create();
 	}
 
