add5fb5e18f50aee481aa860354e76e008d45137
8bit <139103755+RGBXD4@users.noreply.github.com>
2024-07-07 08:40:34 +0800

    Update FreeplayState.hx

--- source/FreeplayState.hx
+++ source/FreeplayState.hx
@@ -345,11 +345,9 @@ class FreeplayState extends MusicBeatState
 
 			var songLowercase:String = Paths.formatToSongPath(songs[curSelected].songName);
 			var poop:String = Highscore.formatSong(songLowercase, curDifficulty);
-			#if MODS_ALLOWED
-			if(!sys.FileSystem.exists(Paths.modsJson(songLowercase + '/' + poop)) && !sys.FileSystem.exists(Paths.json(songLowercase + '/' + poop))) {
-			#else
+			
 			if(!OpenFlAssets.exists(Paths.json(songLowercase + '/' + poop))) {
-			#end
+			
 				poop = songLowercase;
 				curDifficulty = 1;
 				trace('Couldnt find file');
